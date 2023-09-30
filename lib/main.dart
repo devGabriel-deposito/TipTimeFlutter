782f26c93eb57276fe00e069479a04ce85fb78e5
gabriel <gabriel.goncal.freitas@gmail.com>
2023-09-30 14:19:16 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,135 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Hora da Gorjeta",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TipTime(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TipTime extends StatefulWidget {
+  const TipTime({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TipTime> createState() => _TipTimeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TipTimeState extends State<TipTime> {
+  double tipValue = 0.0;
 
-  void _incrementCounter() {
+  TextEditingController valueController = TextEditingController();
+  TextEditingController percentController = TextEditingController();
+  bool roundValue = false;
+
+  void calculateTip() {
+    double value = double.tryParse(valueController.text) ?? 0.0;
+    double percent = double.tryParse(percentController.text) ?? 15.0;
+
+    double calculatedValue = value * (percent / 100);
+    calculatedValue =
+        double.tryParse(calculatedValue.toStringAsFixed(2)) ?? 0.0;
+
+    if (roundValue) {
+      calculatedValue = calculatedValue.ceilToDouble();
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      tipValue = calculatedValue;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Hora da Gorjeta"),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textDirection: TextDirection.ltr,
+          children: [
+            TextField(
+              controller: valueController,
+              onChanged: (value) {
+                calculateTip();
+              },
+              decoration: const InputDecoration(
+                labelText: "Valor",
+                icon: Icon(Icons.monetization_on_rounded),
+              ),
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+            ),
+            TextField(
+              controller: percentController,
+              onChanged: (value) {
+                calculateTip();
+              },
+              decoration: const InputDecoration(
+                labelText: "Porcentagem",
+                icon: Icon(Icons.percent_rounded),
+              ),
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.done,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            Row(
+              children: [
+                // ElevatedButton(
+                //   onPressed: () => calculateTip(),
+                //   child: const Text("Calcular"),
+                // ),
+                const Spacer(flex: 1),
+                Column(
+                  children: [
+                    const Text("Arredondar?"),
+                    Switch(
+                      value: roundValue,
+                      onChanged: (bool value) {
+                        setState(() {
+                          roundValue = value;
+                          calculateTip();
+                        });
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 80.0),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+              "Gorjeta: R\$ ${tipValue}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 32,
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
